1e2d8239c54137c97ccc52a50c1166b53c2ba5e3
Robbe Gillis <robbe.gillis@immalle.eu>
2017-11-25 16:05:53 +0100

    Done

--- MyFileBrowser2/Program.cs
+++ MyFileBrowser2/Program.cs
@@ -1,12 +1,86 @@
 ﻿using System;
-
-namespace MyFileBrowser2
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+namespace MyFileBrowser
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length == 0)
+            {
+                ShowDefault();
+            }
+            else
+            {
+                switch (args[0])
+                {
+                    case "-c":
+                        ShowCurrentDirectory();
+                        break;
+                    case "-f":
+                        ShowAllFilesInCurrentDirectory();
+                        break;
+                    case "-d":
+                        ShowAlleSubDirsInCurrentDir();
+                        break;
+                    case "-h":
+                        ShowHelp();
+                        break;
+
+                    default:
+                        Console.WriteLine("Onbekend argument");
+                        break;
+                }
+            }
         }
+        static void ShowDefault()
+        {
+            Console.WriteLine("Current Directory:");
+            ShowCurrentDirectory();
+            Console.WriteLine("Alle Subdirectories:");
+            ShowAlleSubDirsInCurrentDir();
+            Console.WriteLine("Alle files:");
+            ShowAllFilesInCurrentDirectory();
+        }
+        static void ShowCurrentDirectory()
+        {
+            Console.WriteLine("Current Directory:" + System.IO.Directory.GetCurrentDirectory());
+
+        }
+        static void ShowAllFilesInCurrentDirectory()
+        {
+            string[] directories = Directory.GetFiles(Environment.CurrentDirectory);
+            foreach (string dir in directories)
+            {
+                Int64 fileSizeInBytes = new FileInfo(dir).Length;
+                Console.WriteLine(Path.GetFileName(dir) + "         " + fileSizeInBytes + " bytes");
+            }
+            Console.WriteLine();
+        }
+        static void ShowAlleSubDirsInCurrentDir()
+        {
+            string[] fileArray = Directory.GetDirectories(Environment.CurrentDirectory);
+            for (int i = 0; i < fileArray.Length; i++)
+            {
+
+                Console.WriteLine(fileArray[i]);
+            }
+        }
+        static void ShowHelp()
+        {
+            Console.WriteLine("Help - ofb = MyFileBrowser.exe:");
+            Console.WriteLine("- mfb -c: Toont de huidige directory");
+            Console.WriteLine("- mfb -d: Toont alle subdirectories in  de huidige directory");
+            Console.WriteLine("- mfb -f: Toont alle files in de huidige directory");
+            Console.WriteLine("- mfb: Toont de huidige directory, files en subdirectories");
+            Console.WriteLine("- mfb -h: Toont help");
+        }
+
     }
+
 }
+
+
